611d94a7b19cc6d034de80ba5b02c0430498fb78
Jorgor-prog <alpha9274@outlook.com>
2025-08-19 10:50:45 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export const dynamic = "force-dynamic"
+import { getSession } from "@/lib/cookies"
+import { redirect } from "next/navigation"
+
+export default async function Page() {
+  const s = await getSession()
+  if (s) {
+    redirect(s.user.role === "ADMIN" ? "/admin" : "/dashboard")
+  }
+  redirect("/login")
+}
