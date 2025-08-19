9ffe0a970849115f56e682e73845e52ae7f410ee
Jorgor-prog <alpha9274@outlook.com>
2025-08-20 02:31:30 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-
 import UserTopBar from "@/components/UserTopBar"
 import Image from "next/image"
 import { getSession } from "@/lib/cookies"
@@ -7,7 +6,7 @@ import { redirect } from "next/navigation"
 export default async function Page() {
   const s = await getSession()
   if (!s) redirect("/login")
-  if (s.user.role !== "USER") redirect("/admin")
+  const text = "Ми робимо прості речі надійними, а складні — зрозумілими. Команда працює з увагою до дрібниць, щоб інтерфейси були плавні, швидкі та чесні. Наш підхід — мінімум зайвого, максимум користі. Ми тестуємо, міряємо, оптимізуємо та поважаємо час користувача. Продукт має працювати завжди: на мобільному, повільному інтернеті та з будь-якого куточка світу. Ми будуємо системи, яким можна довіряти, і сервіс, який не підводить. Якщо потрібно — переписуємо, якщо болить — лікуємо, якщо можна — спрощуємо. Тут немає магії: є практика, дисципліна та любов до якості."
   return (
     <div style={{minHeight:"100dvh", position:"relative"}}>
       <div style={{position:"fixed", inset:0, zIndex:-1}}>
@@ -17,7 +16,7 @@ export default async function Page() {
       <div className="container">
         <div className="card">
           <h2>About</h2>
-          <p>Coming soon</p>
+          <p>{text}</p>
         </div>
       </div>
     </div>
